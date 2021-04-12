3d621d7d09a39803fbe2983a8db729a8f7395e4d
Dmitry Malakhov <byuarus@gmail.com>
2021-04-12 14:15:53 -0700

    Initial Commit

---
+++ GStreamerSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GStreamerSwift
+//
+//  Created by Dmytro Malakhov on 4/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
